44944c908f2d59603a79ca388155c1df0a69fab1
Amanda Prorok <amanda.prorok@gmail.com>
2018-09-13 22:55:00 +0100

    .

--- plot_results.py
+++ plot_results.py
@@ -1,12 +1,126 @@
 import argparse
+import collections
 import matplotlib.pylab as plt
 import msgpack
 import msgpack_numpy
+import numpy as np
 import pandas as pd
 import os.path
 import seaborn as sns
 
 import run
+
+
+_RENAME = {
+    'random_priority': 'Random',
+    'surroundings_50_priority': 'Surroundings',
+    'longest_priority': 'Longest first',
+    'forward_looking_priority': 'Forwards looking',
+    # Naive is moved to the tiebreak row.
+    'naive/tiebreak_random': 'N-R',
+    'naive/tiebreak_longest_first': 'N-LF',
+    'tiebreak_random': 'R',
+    'tiebreak_longest_first': 'LF',
+    # Problem sets.
+    'problems/berlin': 'Berlin',
+    'problems/corridor': 'Corridor',
+    'problems/cross': 'Crossing',
+    'problems/multi_corridor': 'Multi-corridor',
+    'problems/successive_clutter': 'Clutter',
+}
+
+
+def compute_stats(df, y):
+  # col_order = list(sorted(df.problem_set.drop_duplicates().values))
+  col_order = [
+      'problems/berlin',
+      'problems/cross',
+      'problems/corridor',
+      'problems/multi_corridor',
+      'problems/successive_clutter',
+  ]
+  # hue_order = list(sorted(df.scheme.drop_duplicates().values))
+  hue_order = [
+      'random_priority+tiebreak_random',
+      'naive_surroundings_50_priority+tiebreak_longest_first',
+      'surroundings_50_priority+tiebreak_longest_first',
+      'longest_priority+tiebreak_random',
+      'naive_forward_looking_priority+tiebreak_longest_first',
+      'forward_looking_priority+tiebreak_random',
+      'forward_looking_priority+tiebreak_longest_first',
+  ]
+  order = list(sorted(df.communication_radius.drop_duplicates().values))
+  g = sns.catplot(x='communication_radius', y=y, hue='scheme', col='problem_set', data=df, kind='bar', col_order=col_order, hue_order=hue_order, order=order)
+
+  means = collections.defaultdict(lambda: collections.defaultdict(dict))
+  stds = collections.defaultdict(lambda: collections.defaultdict(dict))
+  for ax in g.axes.flat:
+    problem_set = ax.title.get_text().split(' = ', 1)[1]
+    assert len(ax.patches) == len(ax.lines)
+    for i, (b, err) in enumerate(zip(ax.patches, ax.lines)):
+      scheme = hue_order[i // len(order)]
+      communication_radius = order[i % len(order)]
+      means[problem_set][communication_radius][scheme] = b.get_height()
+      stds[problem_set][communication_radius][scheme] = err.get_xydata()[:, 1]
+
+  latex = ['\section{%s}' % y.replace('_', ' ')]
+  schemes = collections.defaultdict(list)
+  for scheme in hue_order:
+    priority_scheme, tiebreak_scheme = scheme.split('+', 1)
+    if priority_scheme.startswith('naive_'):
+      priority_scheme = priority_scheme[len('naive_'):]
+      tiebreak_scheme = 'naive/' + tiebreak_scheme
+    if priority_scheme in _RENAME:
+      priority_scheme = _RENAME[priority_scheme]
+    if tiebreak_scheme in _RENAME:
+      tiebreak_scheme = _RENAME[tiebreak_scheme]
+    schemes[priority_scheme].append(tiebreak_scheme)
+
+  # Header.
+  columns = ['c|c||']  # Problem set, Communication radius.
+  for k, v in schemes.items():
+    columns.append('c' * len(v) + '|')
+  columns = ''.join(columns)
+  latex.append('\\begin{tabular}{%s}' % columns)
+  headers = ['\multirow{2}{6em}{Problem set}', '\multirow{2}{1em}{$c$}']
+  for k, v in schemes.items():
+    if len(v) > 1:
+      headers.append('\multicolumn{%d}{|c|}{%s}' % (len(v), k))
+    else:
+      headers.append(k)
+  latex.append(' & '.join(headers) + ' \\\\')
+  headers = ['', '']
+  for k, v in schemes.items():
+    for tb in v:
+      headers.append(tb)
+  latex.append(' & '.join(headers) + ' \\\\')
+  latex.append('\hline')
+
+  # Values.
+  for problem_set in col_order:
+    pb = problem_set
+    if pb in _RENAME:
+      pb = _RENAME[pb]
+    for i, communication_radius in enumerate(order):
+      row = []
+      if i == 0:
+        row.append('\multirow{2}{6em}{%s}' % pb)
+      else:
+        row.append('')
+      row.append(str(int(communication_radius)))
+      values = [means[problem_set][communication_radius][scheme] for scheme in hue_order]
+      min_value = round(min(values), 4)
+      for v in values:
+        if round(v, 4) == min_value:
+          row.append('\\textbf{%.2f\\%%}' % (v * 100.))
+        else:
+          row.append('%.2f\\%%' % (v * 100.))
+      latex.append(' & '.join(row) + ' \\\\')
+    latex.append('\hline')
+  latex.append('\end{tabular}')
+
+  latex = '\n'.join(latex)
+  print(latex)
 
 
 if __name__ == '__main__':
@@ -25,7 +139,10 @@ if __name__ == '__main__':
     for raw_stats in results:
       stats = run.compute_aggregated_statistics(
           run.Statistics(*raw_stats))
-      data.append((os.path.dirname(args.problem), os.path.basename(args.problem),
+      problem_set = os.path.dirname(args.problem)
+      if problem_set.endswith('basic'):
+        continue
+      data.append((problem_set, os.path.basename(args.problem),
                    args.scheme[0] + '+' + args.scheme[1],
                    args.scheme[0], args.scheme[1],
                    args.communication_radius) +
@@ -33,7 +150,7 @@ if __name__ == '__main__':
   df = pd.DataFrame(data, columns=data_columns)
 
   # Plots.
-  for y in ('makespan_ratio', 'flowtime_ratio', 'path_prolongation_ratio', 'success'):
-    sns.catplot(x='communication_radius', y=y, hue='scheme', col='problem_set', data=df, kind='bar')
-    plt.title(y)
+  for y in ('makespan_ratio', 'flowtime_ratio', 'success'):
+    compute_stats(df, y)
+    print('')
   plt.show()
