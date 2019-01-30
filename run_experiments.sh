4c85145ddf2c2b9353a584c688e9278359956ae9
Amanda Prorok <amanda.prorok@gmail.com>
2019-01-30 22:42:20 +0000

    .

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,14 +2,18 @@
 
 python3 run.py \
   --problem=problems/** \
-  --communication_radius=30,50,70 \
+  --communication_radius=30,50 \
   --mode="['dynamic']" \
   --scheme="[('longest_priority', 'tiebreak_random'), \
              ('forward_looking_priority', 'tiebreak_longest_first'), \
              ('forward_looking_priority', 'tiebreak_random'), \
              ('naive_forward_looking_priority', 'tiebreak_longest_first'), \
-             ('surroundings_50_priority', 'tiebreak_longest_first'), \
-             ('naive_surroundings_50_priority', 'tiebreak_longest_first'), \
+             ('surroundings_10_priority', 'tiebreak_longest_first'), \
+             ('naive_surroundings_10_priority', 'tiebreak_longest_first'), \
+             ('surroundings_20_priority', 'tiebreak_longest_first'), \
+             ('naive_surroundings_20_priority', 'tiebreak_longest_first'), \
+             ('surroundings_30_priority', 'tiebreak_longest_first'), \
+             ('naive_surroundings_30_priority', 'tiebreak_longest_first'), \
              ('random_priority', 'tiebreak_random')]" \
   --output_results=results.bin
-# sudo poweroff
+sudo poweroff
